f4d5a09a07ca4f40179222b7a88a32e061eaa1b1
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-02 16:20:41 +0200

    Initial Commit

---
+++ DragGestrures/DragGestruresApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestruresApp.swift
+//  DragGestrures
+//
+//  Created by Vahtee Boo on 02.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct DragGestruresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
